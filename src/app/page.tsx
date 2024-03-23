71dfbd29ca605882d548a31070681be7cfc13fe1
yessine-agrebi <yessine.agrebi.aiesec@gmail.com>
2024-03-23 05:33:51 +0100

    implement code editor + fix ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { ChangeEvent, useEffect, useRef, useState } from "react";
+import CodeEditor from "@uiw/react-textarea-code-editor";
 import CodeBlock from "@/components/CodeBlock";
 import { MinusIcon, PlusCircledIcon, TrashIcon } from "@radix-ui/react-icons";
 import {
@@ -18,11 +19,12 @@ export default function Home() {
 }`,
   ]); // State to store the code
   const [currentSlideIndex, setCurrentSlideIndex] = useState(0); // State to store the current slide index
+  const [textAreaClicked, setTextAreaClicked] = useState(false); // State to store the current slide index
   const textareaRef = useRef<HTMLTextAreaElement>(null); // Ref for the textarea
 
   const handleCodeChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
     const updatedSlides = [...slides]; // Copy the slides array
-    updatedSlides[currentSlideIndex] = event.target.value; // Update the current slide with the edited code
+    updatedSlides[currentSlideIndex] = event.target.value; // Update the current slide with the new code
     setSlides(updatedSlides); // Update the slides state
   };
 
@@ -37,28 +39,22 @@ export default function Home() {
   const handleSetCurrentSlideIndex = (index: number) => {
     setCurrentSlideIndex(index); // Update the current slide index
   };
-  const handleDeleteSlide = () => {
-    console.log("clicked delete");
-    const updatedSlides = slides.filter(
-      (_, index) => index !== currentSlideIndex
-    ); // Remove the current slide from the slides array
-    console.log(updatedSlides);
+  const handleDeleteSlide = (index: number) => {
+    const updatedSlides = slides.filter((_, i) => i !== index); // Filter out the slide to delete
     setSlides(updatedSlides); // Update the slides state
-    setCurrentSlideIndex(updatedSlides.length - 1); // Update the current slide index to the first slide
+    setCurrentSlideIndex(updatedSlides.length); // Update the current slide index to the first slide
   };
 
-  useEffect(() => {
-    console.log({ currentSlideIndex });
-  }, [currentSlideIndex]);
+  
 
   return (
-    <div className="w-full flex flex-col items-center justify-center gap-2">
-      <div className="min-w-[60%] h-[500px] bg-black rounded-lg mt-6 relative">
+    <div className="w-full h-[100vh] flex flex-col items-center justify-center gap-2 bg-slate-800">
+      <div className="min-w-[60%] h-[500px] bg-slate-950 rounded-lg mt-6 relative">
         <div className="w-full h-[50px] flex justify-between items-center px-4 text-lg text-gray-400">
           <VscAzure />
           <div className="flex gap-3 items-center">
-          <VscCircleFilled color="blue" className="mt-1" />
-          <p>index.tsx</p>
+            <VscCircleFilled color="blue" className="mt-1" />
+            <p>index.tsx</p>
           </div>
           <div className="flex items-center gap-2">
             <VscChromeMinimize />
@@ -66,21 +62,20 @@ export default function Home() {
             <VscChromeClose />
           </div>
         </div>
-        {slides.map((slide, index) => (
-          <textarea
-            key={index}
-            ref={index === currentSlideIndex ? textareaRef : null} // Set ref for the active textarea
-            value={slide} // Value bound to the code state
-            onChange={handleCodeChange} // Handle code changes
-            onKeyDown={handleKeyDown} // Handle keydown events
-            className="absolute top-0 left-0 opacity-0 w-full h-full bg-transparent z-10 border-2" // Show the textarea
-            aria-hidden="false" // Hide the textarea from screen readers
-          />
-        ))}
-
-        <CodeBlock code={slides.length > 0 ? slides[currentSlideIndex] : ""} />
+        <CodeEditor
+          value={slides.length > 0 ? slides[currentSlideIndex] : ""}
+          language="js"
+          onChange={handleCodeChange}
+          padding={15}
+          style={{
+            backgroundColor: "transparent",
+            fontFamily:
+              "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
+            fontSize: "15px",
+          }}
+        />
       </div>
-      <div className="min-w-[60%] h-[120px] bg-black rounded-md mt-6 relative flex items-center gap-2">
+      <div className="min-w-[60%] h-[120px] bg-slate-950 rounded-md mt-6 relative flex items-center gap-2">
         <button onClick={handleNewSlide}>
           <PlusCircledIcon className="w-8 h-8 text-white ml-2" />
         </button>
@@ -95,12 +90,24 @@ export default function Home() {
                 className="flex items-center justify-center w-8 h-8 absolute top-0 right-0"
                 onClick={(event) => {
                   event.stopPropagation(); // Prevent event from bubbling up
-                  handleDeleteSlide();
+                  handleDeleteSlide(index);
                 }}
               >
                 <TrashIcon className="text-white text-lg w-6 h-5" />
               </button>
-              <CodeBlock code={slide} />
+              <CodeEditor
+                value={slide}
+                language="tsx"
+                style={{
+                  backgroundColor: "transparent",
+                  fontFamily:
+                    "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
+                  fontSize: "5px",
+                  width: "100%",
+                  height: "100%",
+                  pointerEvents: "none",
+                }}
+              />
             </div>
           ))}
       </div>
